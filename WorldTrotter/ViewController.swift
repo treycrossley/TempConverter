f6d22417c5ca36e97c01fe3a521329433d50eaf3
Trey Crossley <treycrossley@gmail.com>
2020-08-18 16:49:57 -0700

    Initial Commit

---
+++ WorldTrotter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Admin on 8/18/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
