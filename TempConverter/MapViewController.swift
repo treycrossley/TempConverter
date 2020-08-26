742773b7abc7551efe5334e4e061d58b26804d70
Trey Crossley <treycrossley@gmail.com>
2020-08-26 13:14:05 -0700

    points of interest toggle

--- TempConverter/MapViewController.swift
+++ TempConverter/MapViewController.swift
@@ -11,6 +11,7 @@ import MapKit
 class MapViewController: UIViewController {
     
     var mapView: MKMapView!
+    var segmentedControl: UISegmentedControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +23,7 @@ class MapViewController: UIViewController {
         view = mapView
         
         setupSegmentedConstraints()
+        setupSwitchConstraints()
     }
     
     func setupSegmentedConstraints(){
@@ -29,7 +31,7 @@ class MapViewController: UIViewController {
         let hybridString = NSLocalizedString("Hybrid", comment: "Hybrid Map View")
         let satelliteString = NSLocalizedString("Satellite", comment: "Satellite Map View")
         
-        let segmentedControl = UISegmentedControl(items: [standardString, hybridString, satelliteString])
+        segmentedControl = UISegmentedControl(items: [standardString, hybridString, satelliteString])
         segmentedControl.backgroundColor = UIColor.systemBackground
         segmentedControl.selectedSegmentIndex = 0
         segmentedControl.addTarget(self, action: #selector(mapTypeChanged(_:)), for: .valueChanged)
@@ -45,6 +47,45 @@ class MapViewController: UIViewController {
         topConstraint.isActive = true;
         leadingConstraint.isActive = true;
         trailingConstraint.isActive = true;
+    }
+    
+    func setupSwitchConstraints() {
+        let pointOfInterestLabel = UILabel();
+        view.addSubview(pointOfInterestLabel)
+        pointOfInterestLabel.text = NSLocalizedString("Points of Interest", comment: "points of interest along the map")
+        pointOfInterestLabel.textAlignment = .left
+        pointOfInterestLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        let margins = view.layoutMarginsGuide
+        NSLayoutConstraint.activate([
+            pointOfInterestLabel.topAnchor.constraint(equalTo: segmentedControl.bottomAnchor, constant: 12),
+            pointOfInterestLabel.leadingAnchor.constraint(equalTo: margins.leadingAnchor),
+        ])
+        
+        
+        
+        let pointOfInterestToggle = UISwitch()
+        pointOfInterestToggle.isOn = true
+        pointOfInterestToggle.setOn(true, animated: true)
+        pointOfInterestToggle.addTarget(self, action: #selector(pointOfInterestToggled(_:)), for: .valueChanged)
+        pointOfInterestToggle.translatesAutoresizingMaskIntoConstraints = false;
+        view.addSubview(pointOfInterestToggle)
+        
+        NSLayoutConstraint.activate([
+            pointOfInterestToggle.leadingAnchor.constraint(equalTo: pointOfInterestLabel.trailingAnchor, constant: 5),
+            pointOfInterestToggle.bottomAnchor.constraint(equalTo: pointOfInterestLabel.bottomAnchor, constant: 5)
+        ])
+        
+    }
+    
+    @objc func pointOfInterestToggled(_ switchControl: UISwitch){
+        switch switchControl.isOn {
+        case true:
+            mapView.pointOfInterestFilter = MKPointOfInterestFilter.includingAll
+        case false:
+            mapView.pointOfInterestFilter = MKPointOfInterestFilter.excludingAll
+        }
+        
     }
     
     @objc func mapTypeChanged(_ segControl: UISegmentedControl){
